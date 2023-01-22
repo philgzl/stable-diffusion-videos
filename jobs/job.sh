5964415b865ee74525e1961cb0e4803991684281
philgzl <services@philgzl.com>
2023-01-22 21:06:03 +0100

    Update job

--- jobs/job.sh
+++ jobs/job.sh
@@ -5,8 +5,8 @@
 #BSUB -W 24:00
 #BSUB -R "rusage[mem=4GB]"
 #BSUB -R "span[hosts=1]"
-#BSUB -oo logs/%J.out
-#BSUB -eo logs/%J.err
+#BSUB -oo jobs/logs/%J.out
+#BSUB -eo jobs/logs/%J.err
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "select[gpu32gb]"
 export PATH=$PATH:ffmpeg-5.1.1-amd64-static
